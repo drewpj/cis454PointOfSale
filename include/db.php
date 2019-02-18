07b3c9f6a474c48138318bd0e58fea4c02bc2f3f
JGPrish <47034143+JGPrish@users.noreply.github.com>
2019-02-17 19:22:33 -0500

    Add files via upload

---
+++ include/db.php
@@ -0,0 +1,8 @@
+<?php 
+
+$dbServername = "samp454.database.windows.net";
+$dbUsername = "JGPrish";
+$dbPassword = "password!9";
+$dbName = "CIS454";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword,$dbName);
