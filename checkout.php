f841ca531ede68f7e41b99da31f4e1ce48aa3e4d
Drew <drewjacobson@live.com>
2019-02-18 11:15:07 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -8,13 +8,13 @@
 	$familyname = $_POST['familyname'];
 	$email = $_POST['email'];
 	$phone = $_POST['phone'];
-	$addr = $_POST['addr1'] . "\n" . $_POST['addr2'] . "\n" . $_POST['city'] . "," . $_POST['state'] . "\n";
+	$addr = $_POST['addr1'] . "\n" . $_POST['addr2'] . "\n" . $_POST['city'] . "," . $_POST['state'];
 	$zip = $_POST['zip'];
 	
 	$nameB = $_POST['nameBilling'];
 	$familynameB = $_POST['familynameBilling'];
 	$phoneB = $_POST['phoneBilling'];
-	$addrB = $_POST['addr1Billing'] . "\n" . $_POST['addr2Billing'] . "\n" . $_POST['cityBilling'] . "," . $_POST['stateBilling'] . "\n";
+	$addrB = $_POST['addr1Billing'] . "\n" . $_POST['addr2Billing'] . "\n" . $_POST['cityBilling'] . "," . $_POST['stateBilling'];
 	$zipB = $_POST['zipBilling'];
 	
 	$nameCard = $_POST['name-on-card'];
@@ -24,8 +24,8 @@
 	$ccv = $_POST['ccv'];
 	//echo "\n\n\n\n\n" . $ccn . $addrB . $familyname;
 	
-	$shipping = "Shipping information:\n" . $name . " " . $familyname . $nl . $email . $phone . $addr . $nl . $zip;
-	$billing = "Billing information:\n" . $nameB . " " . $familynameB . $nl . $phoneB . $addrB . $nl . $zipB;
+	$shipping = "\nShipping information:\n" . $name . " " . $familyname . $nl . $email . $nl . $phone . $nl . $addr . $nl . $zip;
+	$billing = "\nBilling information:\n" . $nameB . " " . $familynameB . $nl . $phoneB . $nl . $addrB . $nl . $zipB;
 	
 	echo nl2br($shipping . $nl);
 	echo nl2br($billing);
