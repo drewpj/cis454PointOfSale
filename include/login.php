a2e0d68480c85aa5e4953c6e1522a4348fad5857
MahbubarMoon <moonmahbubar@gmail.com>
2019-02-18 18:10:50 -0500

    oylo

--- include/login.php
+++ include/login.php
@@ -20,8 +20,10 @@ $conn = sqlsrv_connect($serverName, $connectionOptions);
 $user_email = $_POST['email'];
 $user_password = $_POST['pwd'];
 
+echo($user_password);
+echo($user_password);
 //hellothere
-$sql = "SELECT user_firstname, user_lastname FROM dbo.User454 WHERE user_email='johndoe@gmail.com' AND PWDCOMPARE('thisisme', user_password) = 1"; //AND PWDCOMPARE(N'$pwd', user_password) = 1";
+$sql = "SELECT user_firstname, user_lastname FROM dbo.User454 WHERE user_email='$user_email' AND PWDCOMPARE('$user_password', user_password) = 1"; //AND PWDCOMPARE(N'$pwd', user_password) = 1";
 //SELECT * FROM User454 WHERE user_email='janedoe@gmail.com' and PWDCOMPARE('hellothere', user_password) = 1
 $result = sqlsrv_query($conn, $sql);
 echo ("Reading data from table" . PHP_EOL);
