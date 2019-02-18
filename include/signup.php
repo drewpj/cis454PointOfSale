e9a31bd2755d52eec4d6bd2cc272167d7fb488aa
Drew <drewjacobson@live.com>
2019-02-17 22:17:20 -0500

    Update signup.php

--- include/signup.php
+++ include/signup.php
@@ -1,7 +1,7 @@
 <?php 
 //include config file for db connection.
 include_once 'db.php';
-
+echo "got down here"
 #Store inputs from the form in php variables.
 $user_type = "customer";
 $user_card = 123456789;
@@ -9,7 +9,7 @@ $user_firstname = mysqli_real_escape_string($conn,$_POST['first']);
 $user_lastname = mysqli_real_escape_string($conn,$_POST['last']);
 $user_email = mysqli_real_escape_string($conn,$_POST['email']);
 $user_password = mysqli_real_escape_string($conn,$_POST['pwd']);
-echo "got down here"
+
 $sql = "INSERT INTO dbo.User454 (user_type, user_card, user_firstname, user_lastname, user_email, user_password) VALUES ('$user_type', '$user_card', '$user_firstname', '$user_lastname', '$user_email', '$user_password')";
 $result = sqlsrv_query($conn, $sql);
 
