eaba03682f407da4f6c1adf9b962b87cd449a4be
MahbubarMoon <moonmahbubar@gmail.com>
2019-02-18 17:02:38 -0500

    null

--- include/login.php
+++ include/login.php
@@ -21,7 +21,7 @@ $uid = $_POST['email'];
 $pwd = $_POST['pwd'];
 
 //hellothere
-$sql = "SELECT user_firstname, user_lastname FROM dbo.User454 WHERE user_email='$uid'"; //AND PWDCOMPARE(N'$pwd', user_password) = 1";
+$sql = "SELECT user_firstname, user_lastname FROM dbo.User454 WHERE user_email=$uid"; //AND PWDCOMPARE(N'$pwd', user_password) = 1";
 //SELECT * FROM User454 WHERE user_email='janedoe@gmail.com' and PWDCOMPARE('hellothere', user_password) = 1
 $result = sqlsrv_query($conn, $sql);
 echo ("Reading data from table" . PHP_EOL);
