3325175adeb0de134aabc05d39d83070f58d2135
Moon <32598682+moonmahbubar@users.noreply.github.com>
2019-02-17 23:21:48 -0500

    Update login.php

--- include/login.php
+++ include/login.php
@@ -5,10 +5,10 @@ include_once 'db.php';
 $uid = mysqli_real_escape_string($conn,$_POST['email']);
 $pwd = mysqli_real_escape_string($conn,$_POST['pwd']);
 
-$sql = "SELECT * FROM dbo.User454 WHERE user_email='$uid' and PWDCOMPARE('$pwd', user_password) = 1";
-$result = mysqli_query($conn, $sql);
+$sql = "SELECT * FROM dbo.User454 WHERE user_email='$uid' AND PWDCOMPARE('$pwd', user_password) = 1";
+$result = mssql_query($conn, $sql);
 
-if (mysqli_num_rows($result) == 1) {
+if (mssql_num_rows($result) == 1) {
     $_SESSION['username'] = $uid;
     header('location: index.php?login=success');
 } else {
