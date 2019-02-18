09ab17e8cd8ee6bd9c50f67de69f50d67002ef62
Moon <32598682+moonmahbubar@users.noreply.github.com>
2019-02-18 13:35:54 -0500

    Testing for SELECT with dbo.User454

--- sqltest.php
+++ sqltest.php
@@ -7,13 +7,15 @@
     );
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT * FROM dbo.Product454";
+    //$tsql= "SELECT * FROM dbo.Product454";
+    //$tsql= "SELECT * FROM dbo.User454";
     $getResults= sqlsrv_query($conn, $tsql);
     echo ("Reading data from table" . PHP_EOL);
     if ($getResults == FALSE)
         echo (sqlsrv_errors());
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-     echo ($row['product_name'] . " " . $row['product_id'] . PHP_EOL);
+     //echo ($row['product_name'] . " " . $row['product_id'] . PHP_EOL);
+     echo ($row['user_firstname'] . " " . $row['user_lastname'] . PHP_EOL);
     }
     sqlsrv_free_stmt($getResults);
 ?>
