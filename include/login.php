e8dd3804aba336b08ca42f07c8d2c6124d5f5596
Moon <32598682+moonmahbubar@users.noreply.github.com>
2019-02-17 23:11:57 -0500

    Update login.php

--- include/login.php
+++ include/login.php
@@ -2,10 +2,10 @@
 session_start();
 include_once 'db.php';
 
-$uid = mysqli_real_escape_string($conn,$_POST['uid']);
+$uid = mysqli_real_escape_string($conn,$_POST['email']);
 $pwd = mysqli_real_escape_string($conn,$_POST['pwd']);
 
-$sql = "select * from users where user_uid='$uid' and user_pwd='$pwd'";
+$sql = "SELECT * FROM dbo.User454 WHERE user_email='$uid' and PWDCOMPARE('$pwd', user_password) = 1";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) == 1) {
@@ -14,5 +14,7 @@ if (mysqli_num_rows($result) == 1) {
 } else {
     header('location: index.php?login=error');
 }
+
+
 
 
