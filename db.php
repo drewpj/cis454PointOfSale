69d78efab4af7a4fd29e7fa2d67835b778c3d10e
JGPrish <47034143+JGPrish@users.noreply.github.com>
2019-02-17 19:21:43 -0500

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$dbServername = "samp454.database.windows.net";
-$dbUsername = "JGPrish";
-$dbPassword = "password!9";
-$dbName = "CIS454";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword,$dbName);
