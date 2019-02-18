c68cd48ea40ccea75f6b64d2a89d962ac9a90940
Drew <drewjacobson@live.com>
2019-02-17 23:32:51 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,5 +1,26 @@
 <?php
 
 	var_dump($_POST);
-
+	echo "Print this recipt out for your records\n";
+	
+	$name = $_POST['name'];
+	$familyname = $_POST['familyname'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$addr = $_POST['addr1'] . "\n" . $_POST['addr2'] . "\n" . $_POST['city'] . "," . $_POST['state'] . "\n";
+	$zip = $_POST['zip'];
+	
+	$nameB = $_POST['nameBilling'];
+	$familynameB = $_POST['familynameBilling'];
+	$phoneB = $_POST['phoneBilling'];
+	$addrB = $_POST['addr1Billing'] . "\n" . $_POST['addr2Billing'] . "\n" . $_POST['cityBilling'] . "," . $_POST['stateBilling'] . "\n";
+	$zipB = $_POST['zipBilling'];
+	
+	$nameCard = $_POST['name-on-card'];
+	$ccn = $_POST['credit-card-number'];
+	$exp_m = $_POST['exp-month'];
+	$exp_y = $_POST['exp-year'];
+	$ccv = $_POST['ccv'];
+	echo "\n\n\n\n\n" . $ccn . addrB . familyname;
+	
 ?>
