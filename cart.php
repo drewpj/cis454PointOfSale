057ba68f2820859f364146d3cb3ac127966d8879
JGPrish <47034143+JGPrish@users.noreply.github.com>
2019-02-17 22:47:44 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -4,7 +4,7 @@ include_once 'include/db.php';
 $id = $_GET['id'];	
 $sql = "SELECT * FROM dbo.Product454";
 $result = sqlsrv_query($conn,$sql);
-/*while ($row = $result->fetch_assoc()) {
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
 	//echo($row['product_name']; 
     if ($row['product_id'] == $id){
 			$price = $row['product_price'];
