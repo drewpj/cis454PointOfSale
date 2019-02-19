0332b6b3bc0545237e4c5e8bd5944e1be9f34153
Bighead454 <47336319+Bighead454@users.noreply.github.com>
2019-02-19 14:23:28 -0500

    Update CIS454 Database.sql

--- CIS454 Database.sql
+++ CIS454 Database.sql
@@ -87,6 +87,20 @@ INSERT INTO Vendor454 (products_sold, vendor_name, vendor_email)
 	VALUES (3, 'Seller1', 'Seller1@gmail.com'), 
 			(6, 'Seller2', 'Seller2@gmail.com'),
 			(6, 'Seller3', 'Seller3@gmail.com') 
+
+INSERT INTO IncomingOrder454 (product_name, product_price, product_seller, orderedBy)
+	VALUES ('Teriyaki Chicken Sub', 18.00, 'Seller3', 'John'),
+    		('The Ultimate Burger', 23.00, 'Seller2', 'LeBron'), 
+            ('Fettaccini in Creamy Sauce', 35.00, 'Seller2', 'Pac'),
+            ('Crab and Prawn Roll with Spinach', 17.00, 'Seller3', 'John'),
+            ('Sushi Platter', 35.00, 'Seller3', 'Lebron'),
+            ('Buffalo Chicken Sandwich', 22.00, 'Seller2', 'Pac'),
+            ('Steak with Seasonal Veggies', 35.00, 'Seller2', 'John'),
+            ('Spaghetti Bolognaise', 27.00, 'Seller2', 'LeBron'),
+            ('Grilled Salmon with Potatoes', 26.00, 'Seller3', 'Pac'),
+            ('Roast Pork with Vegetables', 38.00, 'Seller3', 'John'),
+            ('Garden Pizza', 19.00, 'Seller2', 'LeBron'),
+            ('Spicy Chicken with Rice', 27.00, 'Seller2', 'Pac')											
 	
 -- Adds a primary key to the Payment table 
 ALTER TABLE Payment454
