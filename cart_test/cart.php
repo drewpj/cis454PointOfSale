d66b8bd62cef6d009d9a909f358a4da0f46b35c2
MahbubarMoon <moonmahbubar@gmail.com>
2019-02-18 20:56:58 -0500

    Added comments to cart_test

--- cart_test/cart.php
+++ cart_test/cart.php
@@ -1,21 +1,35 @@
 <?php
 session_start();
+//This entire page is for processing the order and adding it to the cart. 
+//We are sent to this page when the button "Add to cart" is clicked.
 
+//Store the order sent in the variable $order.
 $order =  $_POST['order'];
 
+//Check if the session currenly has 'cart' declared.
 if (!isset($_SESSION['cart'])) {
+    //If not declared, set it to an array.
     $_SESSION['cart'] = array();
+    //Print Message.
     echo("Cart is empty! Initialized to empty cart and added $order! <br> <br>");
+    //Store the reference to the session cart array in $cart. THE & IS IMPORTANT!
     $cart = &$_SESSION['cart']; 
+    //Add the order to the array.
     array_push($cart, $order);
 } else {
+    //If session is declared.
+    //Store the reference to the session cart array in $cart. THE & IS IMPORTANT!
     $cart = &$_SESSION['cart']; 
+    //Add the order to the array.
     array_push($cart, $order);
+    //Print Message.
     echo("Added $order to cart! Here are the items in your cart so far: <br> ");
+    //Use a loop to print out every item in the array.
     foreach($cart as $item) {
         echo($item . "<br>");
     }
 }
 
+//Helpful navigational links.
 echo('<a href="index.html"> go back </a> <br>');
 echo('<a href="clear.php"> clear cart </a>');
--- cart_test/clear.php
+++ cart_test/clear.php
@@ -1,4 +1,7 @@
 <?php
+//Start session. Important!
 session_start();
+//Remove the array from the session. Don't do sesson_destroy() as it'll mess with the login system.
 unset($_SESSION['cart']);
+//Send user back to the main page.
 header("Location: index.html");
