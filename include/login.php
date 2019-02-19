ffb778eab63f0ac39de388c65fb2efd27db228ed
MahbubarMoon <moonmahbubar@gmail.com>
2019-02-18 19:58:25 -0500

    Added comments to include/signup.php and include/login.php

--- include/login.php
+++ include/login.php
@@ -1,4 +1,5 @@
 <?php 
+//start session
 session_start();
 $serverName = "samp454.database.windows.net"; // update me
 $connectionOptions = array(
@@ -6,30 +7,29 @@ $connectionOptions = array(
         "Uid" => "JGPrish", // update me
         "PWD" => "password!9" // update me
 );
-//Establishes the connection
+//Establishes the connection. If succesful, show success message.
 $conn = sqlsrv_connect($serverName, $connectionOptions);
  if($conn) {
     echo "Connected!";
  }
 
+ #Store email and password in php variables.
+$user_email = $_POST['email'];
+$user_password = $_POST['pwd'];
 
-
-// $uid = mysqli_real_escape_string($conn,$_POST['email']);
-// $pwd = mysqli_real_escape_string($conn,$_POST['pwd']);
-
-$user_email = $_GET['email'];
-$user_password = $_GET['pwd'];
-
-//hellothere
+//SQL statement to be queried.
 $sql = "SELECT user_firstname, user_lastname, user_email FROM dbo.User454 WHERE user_email='$user_email' AND PWDCOMPARE('$user_password', user_password) = 1"; //AND PWDCOMPARE(N'$pwd', user_password) = 1";
-//$sql = "SELECT user_firstname, user_lastname FROM dbo.User454 WHERE user_email='johndoe@gmail.com' AND PWDCOMPARE('thisisme', user_password) = 1"; //AND PWDCOMPARE(N'$pwd', user_password) = 1";
-//SELECT * FROM User454 WHERE user_email='janedoe@gmail.com' and PWDCOMPARE('hellothere', user_password) = 1
+
+//Get result from query.
 $result = sqlsrv_query($conn, $sql);
+
+//If returns anything, then login is succesful.
 if (sqlsrv_has_rows($result)) {
     $row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
     echo("Logged in successfully!");
     $_SESSION['email'] = $user_email;
     echo('<a href="../HomePage.php"> go to homepage </a>');
+//Otherwise, say password or username error.
 } else {
 	echo("Wrong password or username!");
 }
--- include/signup.php
+++ include/signup.php
@@ -1,15 +1,10 @@
 <?php 
+//start session
 session_start();
-//include config file for db connection.
+//Include config file for db connection.
 include_once 'db.php';
 
 #Store inputs from the form in php variables.
-//$user_type = "customer";
-//$user_card = 123456789;
-//$user_firstname = mysqli_real_escape_string($conn,$_POST['first']);
-//$user_lastname = mysqli_real_escape_string($conn,$_POST['last']);
-//$user_email = mysqli_real_escape_string($conn,$_POST['email']);
-//$user_password = mysqli_real_escape_string($conn,$_POST['pwd']);
 $user_type = "customer";
 $user_card = 123456789;
 $user_firstname = $_POST['first'];
@@ -17,14 +12,16 @@ $user_lastname = $_POST['last'];
 $user_email = $_POST['email'];
 $user_password = $_POST['pwd'];
 
-// $sql = "INSERT INTO dbo.User454 (user_type, user_card, user_firstname, user_lastname, user_email, user_password) VALUES ('$user_type', '$user_card', '$user_firstname', '$user_lastname', '$user_email', '$user_password')";
-//$sql = "INSERT INTO dbo.User454 VALUES ('$user_type', '$user_card', '$user_firstname', '$user_lastname', '$user_email', '$user_password')";
+//SQL statement that will be queried.
 $sql = "INSERT INTO dbo.User454 (user_type, user_card, user_firstname, user_lastname, user_email, user_password) VALUES ('$user_type', $user_card, '$user_firstname', '$user_lastname', '$user_email', PWDENCRYPT('$user_password'))";
+#Run a query to the connection.
 $result = sqlsrv_query($conn, $sql);
 
+#If query is successful, redirect to login page with signup code success
 if ($result) {
     echo("Success");
     header("Location: ../login.html?signup=success");
+//otherwise, signup code error. **modify later**
 } else {
     echo("error");
     header("Location: ../login.html?signup=error");
