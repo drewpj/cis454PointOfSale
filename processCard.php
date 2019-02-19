e6d955434610236800b128219c07ac8712450355
Drew <drewjacobson@live.com>
2019-02-19 14:40:52 -0500

    Update processCard.php

--- processCard.php
+++ processCard.php
@@ -3,6 +3,7 @@
 
 	function processCard($name,$cnn,$ccv,$month,$year){
 		//this is where we'd process the card if we wanted to pay for that
+		echo " strlen: " . strlen($ccn);
 		if (!strlen($ccn) == 16){
 			return false;
 		}
