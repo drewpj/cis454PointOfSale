54102d6dd2420963b43638c34a7d372a17668b5a
MahbubarMoon <moonmahbubar@gmail.com>
2019-02-19 16:05:16 -0500

    fixed bug in get_orders.php

--- manage_test/get_orders.php
+++ manage_test/get_orders.php
@@ -3,9 +3,9 @@ session_start();
 include_once '../include/db.php';
 
 $sql = "SELECT * FROM dbo.IncomingOrder454";
-$result = sqlsrv_query($conn, $tsql);
+$result = sqlsrv_query($conn, $sql);
 if ($result) {
-    echo("Printing results: <br> <br>")
+    echo("Printing results: <br> <br>");
     while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
         echo ($row['product_name'] . " " . $row['product_id'] . PHP_EOL);
         echo ("<br>");
