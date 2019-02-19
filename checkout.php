6d1f58c8913856414eaa5290b2608d44f3ac0a61
Drew <drewjacobson@live.com>
2019-02-19 15:30:58 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -39,7 +39,7 @@
 	echo nl2br($shipping . $nl);
 	echo nl2br($billing);
 	echo nl2br($charged);
-	echo nl2br($nl . $cnn . $nl);
+	echo nl2br("the card number is: " . $cnn . $nl);
 	if (processCard($nameCard,$cnn,$ccv,$exp_m,$exp_y)){
 		echo "Print this recipt out for your records\n";
 		echo nl2br($shipping . $nl);
