a5b1898d7dfbec25dd8d573887dfc2685c78a5ae
Drew <drewjacobson@live.com>
2019-02-19 17:58:35 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -7,7 +7,7 @@
 	$amount = $_SESSION['total'];
 	$items = $_SESSION['cart'];
 	$itemName = $_SESSION['prodName'];
-	//$orderedBy = $_SESSION['email']
+	$orderedBy = $_SESSION['prodName'];
 	
 	$nl = "\n";
 	
@@ -43,7 +43,7 @@
 		echo nl2br($shipping . $nl);
 		echo nl2br($billing);
 		echo nl2br($charged);
-		$sql = "INSERT INTO dbo.IncomingOrder454 (product_name, product_price, product_seller ,orderedBy) VALUES ('$itemName',floatval($amount),'LeBron','$name')";
+		$sql = "INSERT INTO dbo.IncomingOrder454 (product_name, product_price, product_seller ,orderedBy) VALUES ('$itemName',floatval($amount),'LeBron','$orderedBy')";
 		$result = sqlsrv_query($conn, $sql);
 		if ($result) {
 			echo "success";
