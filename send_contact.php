84ca3f84d29bdd9f88b9bd99b506816e889f43a1
Drew <drewjacobson@live.com>
2019-02-19 20:54:58 -0500

    Update send_contact.php

--- send_contact.php
+++ send_contact.php
@@ -1,27 +1,27 @@
 <?php
 
-
-/* Defining a PHP Function */
-	//var_dump($_GET); //Use this to see what info we got!
-	 function writeMessage() {
-		 //echo "You are really a nice person, Have a nice time!";
-		
+	 function sendMessage() {
+		//getting info from the GET method
 		 $name = $_GET['name'];
 		 $email = $_GET['email'];
 		 $message = $_GET['message'];
 
+		//Initializing variables
 		 $message = "You have received a request from " . $name . " at email " . $email . " saying " . $message . "\n";
-		 //echo $message;
-		 $form_email = "djacobso@syr.edu";
+		 $form_email = "admin@groupthirteen.syr.edu";
 		 $subject_line = "Contact Form Submitted";
 		 $header = "From: " . $email;
+		 
+		 //sending the email
 		 mail($form_email,$subject_line,$message,$header);
+		 
+		 //Notifying the user of us sending the email
 		 echo "Thank you for your comments!"; 
+		 echo nl2br("\nYour comments have been emailed to " . $form_email);
+		 echo nl2br("\nWe have your email on record, and will reply if it is necessary.");
 	 }
-	 /* Calling a PHP Function */
-	 writeMessage();
 	 
-	 //header("Location:  http://www.google.com/");
+	 sendMessage();
   exit();
 
 ?>
